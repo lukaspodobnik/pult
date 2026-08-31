47ec5bfff40b1296438d300db5628ff657e383c7
Lukas Podobnik <lukas.podobnik@gmail.com>
2026-08-31 14:03:49 +0200

    gehört zum letzten commit

--- src/schooltools_tui/screens/school_class_setup.py
+++ src/schooltools_tui/screens/school_class_setup.py
@@ -4,6 +4,8 @@ from textual.containers import Vertical
 from textual.widgets import Button, Footer, Header, Input, Label, SelectionList
 from textual.widgets.selection_list import Selection
 
+from schooltools_tui.initialization.school_class import initialize_school_class
+from schooltools_tui.school_class import SchoolClass
 from schooltools_tui.screens.base import SchooltoolsScreen
 from schooltools_tui.subject import load_subjects
 
@@ -23,9 +25,15 @@ class SchoolClassSetup(SchooltoolsScreen):
     def get_subject_selections(self) -> list[Selection]:
         subjects = load_subjects(self.app_config.root)
         return [
-            Selection(f"{subject.name}", id=f"{subject.id}") for subject in subjects
+            Selection(f"{subject.name}", f"{subject.id}", id=f"{subject.id}") for subject in subjects
         ]
 
     @on(Button.Pressed, "#submit-class")
     def submit_class(self) -> None:
-        selections = self.query_one("#subjects")
+        school_class_id_input = self.query_one("#class-name", Input)
+        subjects = self.query_one("#subjects", SelectionList).selected
+        school_class = SchoolClass(id=school_class_id_input.value, subject_ids=subjects)
+        initialize_school_class(self.app_config.root, self.app_config.active_school_year, school_class)
+
+
+
