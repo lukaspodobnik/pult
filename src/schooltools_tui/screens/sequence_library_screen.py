f2e436c7c2bb907a13805866e98b4f2d5ec5427e
Lukas Podobnik <lukas.podobnik@gmail.com>
2026-09-03 14:07:57 +0200

    editor als subprocess öffnen - in suspend textual

--- src/schooltools_tui/screens/sequence_library_screen.py
+++ src/schooltools_tui/screens/sequence_library_screen.py
@@ -1,12 +1,14 @@
+import shlex
+import subprocess
 from typing import ClassVar
-
+from textual.widgets.tree import TreeNode
 from textual import on
 from textual.app import ComposeResult
 from textual.containers import Container, Horizontal, Vertical
 from textual.widgets import Footer, Header, Label, Tree
 
 from schooltools_tui.screens.base_screen import SchooltoolsScreen
-from schooltools_tui.sequence import load_sequence_library
+from schooltools_tui.sequence import Sequence, get_sequence_path, load_sequence, load_sequence_library
 from schooltools_tui.subject import load_subjects
 from schooltools_tui.widgets.sequence_preview import SequencePreview
 from schooltools_tui.widgets.sequence_tree import SequenceTree
@@ -48,3 +50,33 @@ class SequenceLibraryScreen(SchooltoolsScreen[None]):
 
         preview = self.query_one("#sequence-preview", SequencePreview)
         preview.show_sequence(sequence)
+
+    @on(Tree.NodeSelected, "#sequence-tree")
+    def sequence_selected(self, event: Tree.NodeSelected) -> None:
+        self.open_sequence_in_editor(event.node)
+
+    def open_sequence_in_editor(self, node: TreeNode[Sequence | None]) -> None:
+        sequence = node.data
+        if sequence is None:
+            return
+
+        config = self.app_config
+
+        command = [
+            *shlex.split(config.editor),
+            get_sequence_path(
+                config.root, sequence.grade_level, sequence.subject_id, sequence.id
+            ),
+        ]
+
+        with self.app.suspend():
+            subprocess.run(command, check=False)
+
+        updated_sequence = load_sequence(
+            config.root, sequence.grade_level, sequence.subject_id, sequence.id
+        )
+
+        node.data = updated_sequence
+
+        preview = self.query_one("#sequence-preview", SequencePreview)
+        preview.show_sequence(updated_sequence)
